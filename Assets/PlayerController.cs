22e3c32e8c89de0d7053de10d5795095fa052f59
crimsoncero <crimson.breiman@gmail.com>
2023-04-27 20:48:13 +0300

    1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,54 +24,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (_canMove)
         {
-            Move(Vector2Int.up);
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Move(GridManager.UP);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Move(GridManager.DOWN);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Move(GridManager.LEFT);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Move(GridManager.RIGHT);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Move(Vector2Int.down);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Move(Vector2Int.left);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Move(Vector2Int.right);
-        }
+    
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Animator.SetTrigger("SlashTrigger");
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Animator.SetTrigger("ThrowTrigger");
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Animator.SetTrigger("CastTrigger");
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Animator.SetTrigger("HitTrigger");
-        }
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Animator.SetTrigger("ShotTrigger");
-        }
+       
     }
 
 
 
     private void Move(Vector2Int dir)
     {
+
         Vector2Int newPos = new (_gridPos.x + dir.x, _gridPos.y + dir.y);
 
         if(CombatManager.Instance.CanMove(newPos))
         {
-            Animator.SetTrigger("PhaseTrigger");
+            if(Animator != null) Animator.SetTrigger("PhaseTrigger");
             _gridPos = newPos;
             Transform.position = CombatManager.Instance.GetWorldPos(_gridPos);
         }
