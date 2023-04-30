d6b3587d0c2704241eb34a82a1a60b6b19d5859e
crimsoncero <crimson.breiman@gmail.com>
2023-04-30 23:07:29 +0300

    1

---
+++ Assets/Sprites and Animations/Tilemap/Collisions.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="CollisionMap" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="BaseTiles.png" width="320" height="320"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.0625" y="7.8125">
+    <polygon points="0,0 -14.25,7.75 -30.5625,-0.125 -30.5625,16.9375 -14.5625,24.4375 0.6875,16.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
