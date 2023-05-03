d24dec09cf3b185119f7467d339b129723a6dc1c
Amit Breiman <93070078+crimsoncero@users.noreply.github.com>
2023-05-03 16:34:49 +0300

    1

--- Assets/Sprites and Animations/Tilemap/Palettes/CollisionTileSet.tsx
+++ Assets/Sprites and Animations/Tilemap/Palettes/CollisionTileSet.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Collision" tilewidth="32" tileheight="32" tilecount="100" columns="10" backgroundcolor="#000000">
+<tileset version="1.10" tiledversion="1.10.1" name="Collision" tilewidth="32" tileheight="32" tilecount="100" columns="10">
  <grid orientation="isometric" width="32" height="32"/>
  <image source="CollisionTiles.png" width="320" height="320"/>
  <tile id="0">
