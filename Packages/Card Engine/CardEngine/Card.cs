0aaef003f3e217aed4fc68a8ca6cd78e07ebf713
Amit Breiman <crimson.breiman@gmail.com>
2024-04-21 12:33:45 +0300

    1

---
+++ Packages/Card Engine/CardEngine/Card.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CardEngine
+{
+    public abstract class Card
+    {
+        public int ID { get; private set; }
+
+        public Card (int id)
+        {
+
+            ID = id;
+        }
+    }
+}
